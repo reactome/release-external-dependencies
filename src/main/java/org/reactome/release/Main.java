8d9c4bfc7b7f55a92451ead70f5ffabe52714532
Joel Weiser <joel.weiser@oicr.on.ca>
2020-07-22 20:19:13 -0400

    updates Main class to produce output for debugging

--- src/main/java/org/reactome/release/Main.java
+++ src/main/java/org/reactome/release/Main.java
@@ -1,16 +1,29 @@
 package org.reactome.release;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
+import org.reactome.release.resourcechecker.FTPFileResourceChecker;
+import org.reactome.release.resourcechecker.FileResourceChecker.ByteUnit;
+import org.reactome.release.resourcechecker.ResourceChecker;
+import org.reactome.release.resourcechecker.ResourceCheckerFactory;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
+		System.out.println("ResourceExists\tFileSize");
 		for (Resource resourceToCheck: getResourcesToCheck("External_Resources2.csv")) {
-			ResourceChecker resourceChecker = ResourceChecker.getInstance(resourceToCheck);
+			ResourceChecker resourceChecker = ResourceCheckerFactory.getInstance(resourceToCheck);
 //			System.out.println("Host: " + resourceToCheck.getResourceURL().getHost());
-//			if (resourceChecker == null || !resourceToCheck.getResourceURL().getHost().equals("www.uniprot.org"))
-//				continue;
-//			System.out.println(resourceToCheck.getResourceURL() + ": " + resourceChecker.resourceExists());
+//			System.out.println(resourceToCheck.getResourceURL() + " exists? " + resourceChecker.resourceExists());
+			System.out.println(resourceChecker instanceof FTPFileResourceChecker ? ((FTPFileResourceChecker) resourceChecker).getFileSizeAs(
+				ByteUnit.MEGABYTE) : "");
+			if (resourceChecker instanceof FTPFileResourceChecker) {
+				((FTPFileResourceChecker) resourceChecker).saveFileContents(
+					Paths.get(resourceChecker.getResource().getResourceName()));
+			}
+
+
+			System.out.println(resourceChecker.getReport());
 		}
 	}
 
