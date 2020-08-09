05360f9df6ce6309ed5da591d8a1944742343761
Joel Weiser <joel.weiser@oicr.on.ca>
2020-08-09 17:37:56 -0400

    adds ability to parse JSON array or resource into list of Resource objects

--- src/main/java/org/reactome/release/ResourceBuilder.java
+++ src/main/java/org/reactome/release/ResourceBuilder.java
@@ -1,9 +1,17 @@
 package org.reactome.release;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
@@ -12,13 +20,51 @@ import org.apache.commons.csv.CSVRecord;
 
 public class ResourceBuilder {
 
-	public List<Resource> getResources(String csvResourceFilePath) throws IOException {
-		CSVParser csvParser = CSVParser.parse(Paths.get(csvResourceFilePath), StandardCharsets.UTF_8, CSVFormat.DEFAULT.withHeader());
-		List<CSVRecord> csvRecords = csvParser.getRecords();
+	/**
+	 * Returns a list of Resource objects representing resources as described in a CSV or JSON file.
+	 *
+	 * @param resourceFilePath The path to a CSV of JSON file describing the resources for which to get a list of
+	 * Resource objects.
+	 * @return A list of Resource objects
+	 * @throws IOException Thrown if the file path passed to the resourceFilePath can not be found or if the file can
+	 * not be parsed
+	 * @throws IllegalArgumentException Thrown if the resourceFilePath parameter passed does not end with
+	 * a ".csv" or a ".json" extension
+	 */
+	public List<Resource> getResources(String resourceFilePath) throws IOException {
+		if (resourceFilePath.endsWith(".json")) {
+			return getResourcesFromJSONFile(resourceFilePath);
+		} else if (resourceFilePath.endsWith(".csv")) {
+			return getResourcesFromCSVFile(resourceFilePath);
+		} else {
+			throw new IllegalArgumentException("Extension for the file " + resourceFilePath + " is not recognized");
+		}
+	}
+
+	private List<Resource> getResourcesFromJSONFile(String jsonResourceFilePath) throws FileNotFoundException {
+		JsonParser jsonParser = new JsonParser();
+		JsonArray jsonResourceArray = jsonParser.parse(new FileReader(jsonResourceFilePath)).getAsJsonArray();
+		Iterator<JsonElement> jsonResourceIterator = jsonResourceArray.iterator();
 
 		List<Resource> resources = new ArrayList<>();
-		for (CSVRecord csvRecord : csvRecords) {
-			resources.add(new Resource(csvRecord));
+		while (jsonResourceIterator.hasNext()) {
+			JsonObject jsonResourceObject = jsonResourceIterator.next().getAsJsonObject();
+			resources.add(new Resource(jsonResourceObject));
+		}
+		return resources;
+	}
+
+	private List<Resource> getResourcesFromCSVFile(String csvResourceFilePath) throws IOException {
+		CSVParser csvParser = CSVParser.parse(
+			Paths.get(csvResourceFilePath),
+			StandardCharsets.UTF_8,
+			CSVFormat.DEFAULT.withHeader()
+		);
+		List<CSVRecord> csvResourceRecords = csvParser.getRecords();
+
+		List<Resource> resources = new ArrayList<>();
+		for (CSVRecord csvResourceRecord : csvResourceRecords) {
+			resources.add(new Resource(csvResourceRecord));
 		}
 		return resources;
 	}
